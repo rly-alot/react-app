c5fae45334eff1238ec9a9e813cd7bacf3c4e999
Igor Kotljar <kotljar.igor@gmail.com>
2022-08-02 21:11:14 -0700

    MaterialUI setup finalized

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,9 +1,14 @@
+
 import './App.css';
+import React from 'react';
+import { Button } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
         <h1>Hello world</h1>
+          <button>asd</button>
+          <Button>asd</Button>
     </div>
   );
 }
