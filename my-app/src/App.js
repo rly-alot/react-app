e48c34c4c7d258b6131ea538f903b02c23175c4e
Igor Kotljar <kotljar.igor@gmail.com>
2022-08-02 19:27:08 -0700

    New react project

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+         <h1>Hello World :D</h1>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
