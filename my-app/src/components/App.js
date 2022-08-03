db8db72c23fdaa802daa91bcd058d4942d78600e
Igor Kotljar <kotljar.igor@gmail.com>
2022-08-02 19:39:04 -0700

    Structuring. Deleting useless files

---
+++ my-app/src/components/App.js
@@ -0,0 +1,12 @@
+
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello WORLD ITS LOC's PROJECT :D</h1>
+    </div>
+  );
+}
+
+export default App;
